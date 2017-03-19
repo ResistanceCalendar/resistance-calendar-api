3e1611d0b8a51e95f38c4726a3d1da03e4c36bd9
John Watzke <watzkej@gmail.com>
2017-03-18 22:06:14 -0500

    push from official repo master

--- routes/handlers/facebook.js
+++ routes/handlers/facebook.js
@@ -1,9 +1,14 @@
 const Facebook = require('../../lib/facebook');
 
 module.exports.events = {
-  handler: function (request, reply) {
-    Facebook.getEvents(function(err, res) {
-      return reply(res).type('application/json');
-    });
-  }
+    handler: function (request, reply) {
+        Facebook.getEvents(function (err, res) {
+            return reply(res);
+        });
+    },
+    plugins: {
+        hal: {
+            api: 'osdi:events'
+        }
+    }
 };
