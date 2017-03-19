05e4dc4e467925bc2eb904520ca321b4dc3a25ee
John Watzke <watzkej@gmail.com>
2017-03-19 10:46:20 -0500

    Reverted accidental addition of the hal plugin stuff which belong to another changeset

--- routes/handlers/facebook.js
+++ routes/handlers/facebook.js
@@ -1,14 +1,9 @@
 const Facebook = require('../../lib/facebook');
 
 module.exports.events = {
-    handler: function (request, reply) {
-        Facebook.getEvents(function (err, res) {
-            return reply(res);
-        });
-    },
-    plugins: {
-        hal: {
-            api: 'osdi:events'
-        }
-    }
+  handler: function (request, reply) {
+    Facebook.getEvents(function(err, res) {
+      return reply(res).type('application/json');
+    });
+  }
 };
