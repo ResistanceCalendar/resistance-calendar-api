2fef116b5032020d41be4bf0d5859f6892171089
aaghevli <arash.aghevli@gmail.com>
2017-03-20 22:27:50 -0700

    revert

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ require('./lib/database'); // contains side effect for initializing database
 // Create a server with a host and port
 const server = new Hapi.Server({
   cache: [{
-    engine: require('catbox-mongodb'),
     name: 'mongoCache',
+    engine: require('catbox-mongodb'),
     partition: 'cache',
     uri: config.mongoUri
   }]
