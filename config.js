4793e9ccb3ffeaf5244d5588b38b67842c3b0e9c
aaghevli <arash.aghevli@gmail.com>
2017-03-20 18:48:16 -0700

    Revert facebook caching

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 module.exports = {
+<<<<<<< HEAD
   mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
+=======
+  mongoUri: process.env.MONGODB_URI
+>>>>>>> parent of 561956f... Add caching to facebook events endpoint
 };
