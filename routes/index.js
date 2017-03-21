96da868960de18bf93c92379bfe38bc4036aaf3b
aaghevli <arash.aghevli@gmail.com>
2017-03-20 19:57:41 -0700

    Revert caching again

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
 const Facebook = require('./handlers/facebook');
 const Event = require('./handlers/event');
 const Location = require('./handlers/location');
-const moment = require('moment');
-
-/**
- * Setting up caching via help from the following article:
- *   http://vawks.com/blog/2014/03/15/caching-with-hapi/
- *
- * All times in ms:
- *  expiresIn - relative expiration since the item was saved in the cache
- *  staleIn - mark an item stored in cache as stale and attempt to regenerate
- *  staleTimeout - wait before returning a stale value while generateFunc is
- *                 generating a fresh value.
- *  generateTimeout - wait before returning a timeout error when the
- *                    generateFunc function takes too long to return a value
- */
 
 exports.register = (plugin, options, next) => {
-  plugin.method('getFacebookEvents', Facebook.events, {
-    cache: {
-      expiresIn: moment.duration(5, 'minute').asMilliseconds(),
-      staleIn: moment.duration(1, 'minute').asMilliseconds(),
-      staleTimeout: 100,
-      generateTimeout: moment.duration(2, 'seconds').asMilliseconds()
-    }
-  });
-
   plugin.route([
-    createRoute('GET', '/facebook/events', plugin.methods.getFacebookEvents),
+    { method: 'GET', path: '/facebook/events', config: Facebook.events },
     { method: 'POST', path: '/events', config: Event.create },
     { method: 'POST', path: '/location', config: Location.create }
   ]);
@@ -36,16 +13,4 @@ exports.register = (plugin, options, next) => {
 
 exports.register.attributes = {
   name: 'api'
-};
-
-const createRoute = function (method, path, serverMethod) {
-  return {
-    method: method,
-    path: path,
-    handler: function (request, reply) {
-      serverMethod(function (error, result) {
-        reply(error || result);
-      });
-    }
-  };
 };
