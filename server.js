9404b19bd667c62144b93e304cb41de27767a207
aaghevli <arash.aghevli@gmail.com>
2017-03-20 21:24:15 -0700

    Facebook caching

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 'use strict';
 
+const config = require('./config');
+
 const Hapi = require('hapi');
 const corsHeaders = require('hapi-cors-headers');
 require('./lib/database'); // contains side effect for initializing database
 
 // Create a server with a host and port
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  cache: [{
+    name: 'mongoCache',
+    engine: require('catbox-mongodb'),
+    uri: config.mongoUri,
+    partition: 'cache'
+  }]
+});
 server.connection({
   port: process.env.PORT || 8000
 });
