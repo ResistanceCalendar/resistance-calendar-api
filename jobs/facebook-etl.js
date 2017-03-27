e1cfdea31620fc39a0d8dcb303bf66e3dbdad9e3
aaghevli <arash.aghevli@gmail.com>
2017-03-26 20:48:31 -0700

    Allow for save/update

--- jobs/facebook-etl.js
+++ jobs/facebook-etl.js
@@ -1,8 +1,9 @@
+const Event = require('../models/osdi/event');
 const Facebook = require('../lib/facebook');
 require('../lib/database'); // Has side effect of connecting to database
 
 module.exports = function (job, done) {
-  Facebook.getOSDIEvents(function (err, res) {
+  Facebook.getOSDIEvents({per_page: 1}, function (err, res) {
     if (err) {
       console.error(err);
       return;
@@ -11,16 +12,36 @@ module.exports = function (job, done) {
     const osdiEvents = res;
 
     osdiEvents.forEach(function (osdiEvent) {
-      console.log('Saving ' + osdiEvent.name);
-      osdiEvent.save(function (err, data) {
+      console.log('Saving ' + osdiEvent.name + ' (' + osdiEvent.identifiers[0] + ')');
+
+      // TODO(aaghevli): Make sure we pick the right id
+      const query = { identifiers: { $in: [osdiEvent.identifiers[0]] } };
+      Event.find(query, function (err, eventData) {
         if (err) {
-          console.log('error saving' + osdiEvent.name, err);
+          console.log('error finding ' + osdiEvent.name, err);
           throw new Error(err);
         }
-        console.log('saved ' + data.name);
+
+        if (eventData.length === 0) {
+          osdiEvent.save(function (err, data) {
+            if (err) {
+              console.log('error saving' + osdiEvent.name, err);
+              throw new Error(err);
+            }
+            console.log('saved ' + data.name);
+          });
+        } else {
+          osdiEvent._id = eventData._id;
+          Event.update(osdiEvent, function (err, raw) {
+            if (err) {
+              console.log('error updating ' + osdiEvent.name, err);
+              throw new Error(err);
+            }
+            console.log('updated ' + osdiEvent.name);
+          });
+        }
       });
     });
-    console.log('done!');
     done();
   });
 };
