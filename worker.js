3f2145a2b33cbef7161ec7c1b93f1f37add3a5c0
aaghevli <arash.aghevli@gmail.com>
2017-03-26 19:34:04 -0700

    Futz with database

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ const config = require('./config');
 const facebookEtl = require('./jobs/facebook-etl');
 require('./lib/database');  // Has side effect of connecting to database
 
-const agenda = new Agenda({db: {address: config.mongoUri + '/agenda'}});
+const agenda = new Agenda({db: {address: config.mongoUri}});
 
 agenda.define('facebook-etl', facebookEtl);
 
