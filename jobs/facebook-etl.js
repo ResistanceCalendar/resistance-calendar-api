98412e03796e10ebd693ac36e7d52510dda247c9
aaghevli <arash.aghevli@gmail.com>
2017-03-27 20:24:14 -0700

    minor cleanups

--- jobs/facebook-etl.js
+++ jobs/facebook-etl.js
@@ -3,31 +3,9 @@ const Facebook = require('../lib/facebook');
 require('../lib/database'); // Has side effect of connecting to database
 
 module.exports = function (job, done) {
-  Facebook.getOSDIEvents({per_page: 1}, function (err, res) {
+  Facebook.getAllOSDIEvents(function (err, res) {
     if (err) handleError('fetching facebook events', err);
-
-    res.forEach(function (osdiEvent) {
-      const facebookId = osdiEvent.identifiers[0];
-      const facebookEventName = osdiEvent.name + '[' + facebookId + ']';
-
-      // TODO(aaghevli): Make sure we pick the right id
-      const query = { identifiers: { $in: [facebookId] } };
-      Event.find(query, function (err, eventData) {
-        if (err) handleError('error finding ' + facebookEventName, err);
-        if (eventData.length === 0) {
-          osdiEvent.save(function (err, data) {
-            if (err) handleError('error saving' + facebookEventName, err);
-            console.log('saved ' + facebookEventName);
-          });
-        } else {
-          osdiEvent._id = eventData._id;
-          Event.update(osdiEvent, function (err, raw) {
-            if (err) handleError('error updating ' + facebookEventName, err);
-            console.log('updated ' + facebookEventName);
-          });
-        }
-      });
-    });
+    res.forEach(saveOrUpdate);
     done();
   });
 };
@@ -35,4 +13,27 @@ module.exports = function (job, done) {
 const handleError = function (err, str) {
   console.log(str, err);
   throw new Error(err);
+};
+
+const saveOrUpdate = function (osdiEvent) {
+  const facebookId = osdiEvent.identifiers[0];
+  const facebookEventName = osdiEvent.name + ' [' + facebookId + ']';
+
+  // TODO(aaghevli): Make sure we pick the right id
+  const query = { identifiers: { $in: [facebookId] } };
+  Event.find(query, function (err, eventData) {
+    if (err) handleError('error finding ' + facebookEventName, err);
+    if (eventData.length === 0) {
+      osdiEvent.save(function (err, data) {
+        if (err) handleError('error saving' + facebookEventName, err);
+        console.log('saved ' + facebookEventName);
+      });
+    } else {
+      osdiEvent._id = eventData._id;
+      Event.update(osdiEvent, function (err, raw) {
+        if (err) handleError('error updating ' + facebookEventName, err);
+        console.log('updated ' + facebookEventName);
+      });
+    }
+  });
 };
