0d354de2796eb515e9ab371c4b6a92498203bf10
aaghevli <arash.aghevli@gmail.com>
2017-03-31 10:15:08 -0700

    Swap order

--- worker.js
+++ worker.js
@@ -15,7 +15,7 @@ agenda.on('start', function (job) {
 
 agenda.on('ready', function () {
   console.log('Worker processes ready...');
-  agenda.now('facebook-etl');
   agenda.every('1 hour', 'facebook-etl');
+  agenda.now('facebook-etl');
   agenda.start();
 });
