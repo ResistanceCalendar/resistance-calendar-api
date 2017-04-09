967f783b944aaf73b35bb8d6ec28461275315d73
aaghevli <arash.aghevli@gmail.com>
2017-04-08 20:04:08 -0700

    Use Event.findOneAndUpdate (#50)
    * Fix events update
    
    * Add comment

--- jobs/facebook-etl.js
+++ jobs/facebook-etl.js
@@ -23,20 +23,19 @@ const saveOrUpdate = function (osdiEvent) {
   if (facebookId) {
     const facebookEventName = osdiEvent.name + ' [' + facebookId + ']';
     const query = { identifiers: { $in: [facebookId] } };
-    Event.find(query, function (err, eventData) {
-      if (err) handleError('error finding ' + facebookEventName, err);
-      if (eventData.length === 0) {
-        osdiEvent.save(function (err, data) {
-          if (err) handleError('error saving' + facebookEventName, err);
-          console.log('saved ' + facebookEventName);
-        });
-      } else {
-        osdiEvent._id = eventData._id;
-        Event.update(osdiEvent, function (err, raw) {
-          if (err) handleError('error updating ' + facebookEventName, err);
-          console.log('updated ' + facebookEventName);
-        });
-      }
+
+    // This funny bit of code is necessary to clear the existing _id from the
+    // model since the id may not be deterministic at the time of model creation
+    //
+    // See http://stackoverflow.com/questions/31775150/node-js-mongodb-the-immutable-field-id-was-found-to-have-been-altered
+    //
+    var eventToUpdate = {};
+    eventToUpdate = Object.assign(eventToUpdate, osdiEvent._doc);
+    delete eventToUpdate._id;
+
+    Event.findOneAndUpdate(query, eventToUpdate, {upsert: true}, function (err, doc) {
+      if (err) handleError(err);
+      console.log('upserted ' + facebookEventName);
     });
   } else {
     const err = 'no facebook id found ' + osdiEvent.name;
