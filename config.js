b4d2bfe24c7ab94b6591da5c080644199a915fbe
KamillaKhabibrakhmanova <venessiel@gmail.com>
2017-04-15 22:07:36 -0400

    add base url to config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rc_test'
+  mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rc_test',
+  baseUrl: process.env.BASE_URL || 'http://localhost:8000'
 };
