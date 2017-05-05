58a99dff8f14935798d6ab4c4510b11d90bb1b86
aaghevli <arash.aghevli@gmail.com>
2017-05-04 20:07:42 -0700

    Add browser_url

--- worker.js
+++ worker.js
@@ -7,14 +7,4 @@ require('./lib/database');  // Has side effect of connecting to database
 
 const agenda = new Agenda({db: {address: config.mongoUri}});
 
-agenda.define('facebook-etl', facebookEtl);
-
-agenda.on('start', function (job) {
-  console.log('Job %s starting', job.attrs.name);
-});
-
-agenda.on('ready', function () {
-  console.log('Worker processes ready...');
-  agenda.every('1 hour', 'facebook-etl');
-  agenda.start();
-});
+facebookEtl();
