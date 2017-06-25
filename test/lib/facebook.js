8b0197218b5527a0bf04d52ad45f4605eeef134b
aaghevli <arash.aghevli@gmail.com>
2017-06-25 07:05:20 -0700

    add test

--- test/lib/facebook.js
+++ test/lib/facebook.js
@@ -3,12 +3,18 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const Facebook = require('../../lib/facebook');
 
-lab.test('Facebook.toOSDIEvent', (done) => {
-  const facebookEvent = {
+lab.test('Facebook.toOSDIEvent identifiers', (done) => {
+  const osdiEvent = Facebook.toOSDIEvent({
     id: '00000'
-  };
-  const osdiEvent = Facebook.toOSDIEvent(facebookEvent);
+  });
   Code.expect(osdiEvent.origin_system).to.equal('Facebook');
   Code.expect(osdiEvent.identifiers).to.equal(['facebook:00000']);
+  done();
+});
+
+lab.test('Facebook.toOSDIEvent status', (done) => {
+  Code.expect(Facebook.toOSDIEvent({}).status).to.equal('confirmed');
+  Code.expect(Facebook.toOSDIEvent({is_canceled: false}).status).to.equal('confirmed');
+  Code.expect(Facebook.toOSDIEvent({is_canceled: true}).status).to.equal('cancelled');
   done();
 });
