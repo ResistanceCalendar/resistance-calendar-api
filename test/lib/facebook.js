e2fa2773b520123565b418d64437d0ffc62dfcf9
aaghevli <arash.aghevli@gmail.com>
2018-01-03 20:14:10 -0800

    Fix lat long error when undefined

--- test/lib/facebook.js
+++ test/lib/facebook.js
@@ -18,3 +18,23 @@ lab.test('Facebook.toOSDIEvent status', (done) => {
   Code.expect(Facebook.toOSDIEvent({is_canceled: true}).status).to.equal('cancelled');
   done();
 });
+
+lab.test('Facebook.toOSDIEvent location', (done) => {
+  const noLat = {location: {latitude: undefined, longitude: -122.42118}};
+  Code.expect(Facebook.toOSDIEvent({id: '00000', place: noLat}).location.location).to.equal(undefined);
+
+  const noLong = {location: {latitude: 37.76056, longitude: undefined}};
+  Code.expect(Facebook.toOSDIEvent({id: '00000', place: noLong}).location.location).to.equal(undefined);
+
+  const latLong = {location: {latitude: 37.76056, longitude: -122.42118}};
+  Code.expect(Facebook.toOSDIEvent({id: '00000', place: latLong}).location.location).to.equal({
+    longitude: -122.42118,
+    latitude: 37.76056,
+    type: 'Point',
+    coordinates: [
+      -122.42118,
+      37.76056
+    ]
+  });
+  done();
+});
