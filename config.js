bdcd05a0b0469ba8b967027f010079e28ccd4887
aaghevli <arash.aghevli@gmail.com>
2018-01-06 19:12:02 -0800

    reduce ttl to 30 days

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   maxPageSize: process.env.MAX_PAGE_SIZE || 25,
   mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rc_test',
   s3ImageBucket: process.env.S3_BUCKET_NAME,
-  eventTimeToLiveMs: 90 * 24 * 60 * 60 * 1000,
+  eventTimeToLiveMs: 30 * 24 * 60 * 60 * 1000,
   facebookGraphApiVersion: process.env.FB_GRAPH_API_VERSION || '2.8',
   facebookGraphApiToken: process.env.FB_GRAPH_API_TOKEN,
   slackEndpoint: process.env.SLACK_ENDPOINT
