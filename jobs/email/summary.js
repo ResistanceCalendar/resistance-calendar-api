e662cf3add9f52c58f4364151703e5aaf4b29064
aaghevli <arash.aghevli@gmail.com>
2018-03-06 21:03:54 -0800

    Rename email job and fix formatting and links

--- jobs/email/summary.js
+++ jobs/email/summary.js
@@ -6,6 +6,7 @@ require('../../lib/database'); // Has side effect of connecting to database
 
 const upcomingEventsQuery = Event.find({start_date: {$gte: Date.now()}});
 const PastDay = Date.now() - (24 * 60 * 60 * 1000);
+const ThisWeek = Date.now() + (7 * 24 * 60 * 60 * 1000);
 
 upcomingEventsQuery
   .catch(function (err) { if (err) console.error(err); })
@@ -20,12 +21,19 @@ upcomingEventsQuery
           event.created_date.getTime() > PastDay;
       });
 
+    const eventsThisWeek = upcomingEvents.filter(function (event) {
+      return event.start_date &&
+        event.start_date.getTime() > Date.now() &&
+        event.start_date.getTime() < ThisWeek;
+    });
+
     var stats = {
       email: 'arash.aghevli@gmail.com',
       subject: 'Resistance Calendar this week',
       recentlyAddedEvents: _.sortBy(recentlyAddedEvents, (e) => e.start_date),
       upcomingEventCount: upcomingEventCount,
-      upcomingRSVPCount: upcomingRSVPCount
+      upcomingRSVPCount: upcomingRSVPCount,
+      eventsThisWeek: _.sortBy(eventsThisWeek, (e) => e.start_date)
     };
 
     Sendemail.email('summary', stats, function (err, result) {
