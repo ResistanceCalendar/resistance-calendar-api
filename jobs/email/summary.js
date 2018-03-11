97b9b72f502289b0436bae023d8f14aff360b6ca
aaghevli <arash.aghevli@gmail.com>
2018-03-10 22:05:34 -0800

    Fix date column

--- jobs/email/summary.js
+++ jobs/email/summary.js
@@ -1,17 +1,23 @@
 const Sendemail = require('sendemail');
 const Event = require('../../models/osdi/event');
 const config = require('../../config.js');
+const moment = require('moment');
 const _ = require('lodash');
 
 require('../../lib/database'); // Has side effect of connecting to database
 
 const upcomingEventsQuery = Event.find({start_date: {$gte: Date.now()}});
 const PastDay = Date.now() - (24 * 60 * 60 * 1000);
-const ThisWeek = Date.now() + (7 * 24 * 60 * 60 * 1000);
+const UpcomingThreshold = Date.now() + (3 * 24 * 60 * 60 * 1000);
 
 upcomingEventsQuery
   .catch(function (err) { if (err) console.error(err); })
-  .then(function (upcomingEvents) {
+  .then(function (rawUpcomingEvents) {
+    const upcomingEvents = rawUpcomingEvents.map(function (event) {
+      event.start_date_string = event.start_date ? moment(event.start_date).format('MMMM Do YYYY, h:mm:ss a') : undefined;
+      return event;
+    });
+
     const upcomingEventCount = upcomingEvents.length;
     const upcomingRSVPCount = _.sumBy(upcomingEvents, function (event) {
       return event.total_accepted;
@@ -25,7 +31,7 @@ upcomingEventsQuery
     const eventsThisWeek = upcomingEvents.filter(function (event) {
       return event.start_date &&
         event.start_date.getTime() > Date.now() &&
-        event.start_date.getTime() < ThisWeek;
+        event.start_date.getTime() < UpcomingThreshold;
     });
 
     var stats = {
@@ -34,7 +40,7 @@ upcomingEventsQuery
       recentlyAddedEvents: _.sortBy(recentlyAddedEvents, (e) => e.start_date),
       upcomingEventCount: upcomingEventCount,
       upcomingRSVPCount: upcomingRSVPCount,
-      eventsThisWeek: _.sortBy(eventsThisWeek, (e) => e.start_date)
+      upcomingEvents: _.sortBy(eventsThisWeek, (e) => e.start_date)
     };
 
     Sendemail.email('summary', stats, function (err, result) {
