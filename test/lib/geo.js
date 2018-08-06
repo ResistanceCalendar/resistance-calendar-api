8ebf6436d864d4024a9e00e9f10cd3b3d1c8787b
aaghevli <arash.aghevli@gmail.com>
2018-08-05 20:34:05 -0700

    Do the lat/long lookup at the end, and also determine from city/state

--- test/lib/geo.js
+++ test/lib/geo.js
@@ -8,14 +8,14 @@ lab.test('Geo.parseAddressStringToOSDILocation', (done) => {
   Geo.parseAddressStringToOSDILocation(addressLine, function (err, osdiLocation) {
     if (err) Code.fail(err);
 
-    Code.expect(osdiLocation.location.type, 'Point');
+    Code.expect(osdiLocation.location.type).to.equal('Point');
     Code.expect(osdiLocation.location.latitude).to.equal(26.6182);
     Code.expect(osdiLocation.location.longitude).to.equal(-80.056);
     Code.expect(osdiLocation.location.coordinates).to.equal([-80.056, 26.6182]);
 
-    Code.expect(osdiLocation.postal_code).to.equal('33460');
-    Code.expect(osdiLocation.region).to.equal('FL');
     Code.expect(osdiLocation.address_lines[0]).to.equal('100 S Gulfview Rd');
+    Code.expect(osdiLocation.region).to.equal('FL');
+    Code.expect(osdiLocation.postal_code).to.equal('33460');
     done();
   });
 });
@@ -25,6 +25,23 @@ lab.test('Geo.parseAddressStringToOSDILocation fixes missing state', (done) => {
   Geo.parseAddressStringToOSDILocation(addressLine, function (err, osdiLocation) {
     if (err) Code.fail(err);
     Code.expect(osdiLocation.region).to.equal('IL');
+    done();
+  });
+});
+
+lab.test('Geo.parseAddressStringToOSDILocation fixes missing zip', (done) => {
+  const addressLine = '1245 Some Street, Wichita, KS';
+  Geo.parseAddressStringToOSDILocation(addressLine, function (err, osdiLocation) {
+    if (err) Code.fail(err);
+    Code.expect(osdiLocation.location.type).to.equal('Point');
+    Code.expect(osdiLocation.location.latitude).to.equal(37.6922);
+    Code.expect(osdiLocation.location.longitude).to.equal(-97.3375);
+    Code.expect(osdiLocation.location.coordinates).to.equal([-97.3375, 37.6922]);
+
+    Code.expect(osdiLocation.address_lines[0]).to.equal('1245 Some Street');
+    Code.expect(osdiLocation.locality).to.equal('Wichita');
+    Code.expect(osdiLocation.region).to.equal('KS');
+    Code.expect(osdiLocation.postal_code).to.equal('67201');
     done();
   });
 });
